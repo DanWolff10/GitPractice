d5994a062fe557d9fbb30f4cead7e543848518fd
Daniel Wolff <danielwolff199@gmail.com>
2024-02-27 14:47:53 +0200

    fixed merge conflict

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -4,9 +4,6 @@ int main(){
     std::cout<<"hello world!\n";
     std::cout<<"My name is Daniel Wolff";
     std::cout<<"we are working on the branch abc";
-<<<<<<< HEAD
     std::cout<<"one";
-=======
     std::cout<<"TWO";
->>>>>>> two
 }
